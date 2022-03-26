b8da5d3901e31e64ddcbc7057c9358805275d8a3
Syd Amir <hemedani@gmail.com>
2022-03-26 14:46:48 +0430

    add mod on structs folder

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,3 @@
 export * from "./error.ts";
 export * from "./struct.ts";
-export * from "./structs/coercions.ts";
-export * from "./structs/refinements.ts";
-export * from "./structs/types.ts";
-export * from "./structs/utilities.ts";
+export * from "./structs/mod.ts";
